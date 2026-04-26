096a7f5c9fdec8ff04ad79f4e56264fa6aef3f7c
noor <noorzohbi8@gmail.com>
2026-04-26 18:09:15 +0300

    Integrated Material UI library into React TypeScript project

--- react-pj/src/main.tsx
+++ react-pj/src/main.tsx
@@ -4,6 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
