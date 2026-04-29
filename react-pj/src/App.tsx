398deaeed7052227c3a01dd17f2c7e6317296572
noor <noorzohbi8@gmail.com>
2026-04-29 09:24:03 +0300

    Add login page with SCSS styling

--- react-pj/src/App.tsx
+++ react-pj/src/App.tsx
@@ -1,34 +1,5 @@
-import SlideBar from './components/SlideBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MyNavbar from './components/NavBar';
-
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import Products from './pages/Products';
-import Contact from './pages/Contact';
-function App() {
-  return (
-    
-    <Router>
-<SlideBar />
-      <div style={{ display: 'flex' }}>
-         {/* Main content area */}
-        <div style={{ width: '100%' }}>
-          <MyNavbar />
-
-          <div style={{ padding: '20px' }}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </div>
-        </div>
-
-      </div>
-    </Router>
-    
-  );
-}
+import Login from "./features/auth/login/login"
+const App  = () =>{
+  return <Login/>;
+};
 export default App;
